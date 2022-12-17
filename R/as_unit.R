907b0461a5dc32d835bff8b0970b185116f43b4f
Eli Pousson <eli.pousson@gmail.com>
2022-12-17 10:39:34 -0500

    feat(as_unit): export helper functions for grid units

---
+++ R/as_unit.R
@@ -0,0 +1,151 @@
+
+#' Helper functions for grid units
+#'
+#' @description
+#' - [as_unit()]: Convert to unit (allowing unit objects as units)
+#' - [as_unit_type()]: Convert to unit type (or checking unit types)
+#' - [convert_unit_type()]: Convert x from one unit type to another (preserving
+#' names for named vectors)
+#' - [is_same_unit_type()]: Are x and y the same unit type?
+#'
+#' @name as_unit
+#' @examples
+#'
+#' inch <- as_unit(1, "in")
+#'
+#' inch
+#'
+#' as_unit(10, inch)
+#'
+#' as_unit(inch, "cm")
+#'
+#' as_unit(inch)
+#'
+#' convert_unit_type(inch, to = "cm")
+#'
+#' convert_unit_type(c(10, 100), from = "mm", to = "cm")
+#'
+#' is_same_unit_type(inch, "in")
+#'
+#' is_same_unit_type("pt", "points")
+#'
+#' @inheritParams grid::unit
+#' @inheritParams cli::cli_abort
+#' @export
+as_unit <- function(x,
+                    units = NULL,
+                    data = NULL,
+                    recurse = FALSE,
+                    arg = caller_arg(x),
+                    call = parent.frame()) {
+  rlang::try_fetch(
+    grid::unit(as.numeric(x), as_unit_type(units %||% x, recurse), data),
+    error = function(cnd) {
+      cli::cli_abort(
+        "{.arg {arg}} can't be coerced to a unit object.",
+        call = call,
+        parent = cnd
+      )
+    }
+  )
+}
+
+#' @name as_unit_type
+#' @rdname as_unit
+#' @inheritParams grid::unitType
+#' @export
+#' @importFrom rlang caller_arg try_fetch
+#' @importFrom grid unitType unit
+#' @importFrom cli cli_abort
+as_unit_type <- function(x,
+                         recurse = FALSE,
+                         arg = caller_arg(x),
+                         call = parent.frame()) {
+  if (is_unit(x)) {
+    return(grid::unitType(x, recurse))
+  }
+
+  rlang::try_fetch(
+    grid::unitType(grid::unit(1, x), recurse),
+    error = function(cnd) {
+      cli::cli_abort(
+        "{.arg {arg}} can't be coerced to a unit type.",
+        call = call,
+        parent = cnd
+      )
+    }
+  )
+}
+
+#' @name convert_unit_type
+#' @rdname as_unit
+#' @param from Unit to convert from. If `NULL` and x is not a units object,
+#'   convert to `to` units with a warning.
+#' @param to Unit to convert to. Passed to unitTo parameter of
+#'   [grid::convertUnit()]. If `NULL`, return x as is.
+#' @param typeFrom Passed to typeFrom parameter of [grid::convertUnit()].
+#'   Defaults to "dimension".
+#' @param valueOnly Passed to valueOnly parameter of [grid::convertUnit()].
+#'   Defaults to `FALSE`.
+#' @inheritDotParams grid::convertUnit -unitTo
+#' @export
+#' @importFrom rlang is_named set_names
+#' @importFrom cli cli_abort
+#' @importFrom grid unit convertUnit
+convert_unit_type <- function(x,
+                              from = NULL,
+                              to = NULL,
+                              typeFrom = "dimension",
+                              valueOnly = FALSE,
+                              ...) {
+  if (is.null(to)) {
+    return(x)
+  }
+
+  nm <- NULL
+
+  if (rlang::is_named(x)) {
+    nm <- names(x)
+    x <- rlang::set_names(x, NA)
+  }
+
+  if (!is_unit(x) && is.null(from)) {
+    cli::cli_warn(
+      "{.arg from} is {.code NULL}, converting {.arg x} to {.val {to}}."
+    )
+    return(rlang::set_names(as_unit(x, to), nm))
+  }
+
+  if (is_unit(x) && is_unit(from) && !is_same_unit_type(x, from)) {
+    cli::cli_warn(
+      "Existing  {.arg x} unit type {.val {as_unit_type(x)}} is ignored
+        when {.arg from} is a {.cls unit} object."
+    )
+  }
+
+  x <- as_unit(x, from)
+
+  if (!is_unit(x)) {
+    cli::cli_abort(
+      "{.arg x} must be a {.cls numeric} vector or {.cls unit} object."
+    )
+  }
+
+  x <-
+    grid::convertUnit(
+      x,
+      unitTo = as_unit_type(to),
+      valueOnly = valueOnly,
+      typeFrom = typeFrom,
+      ...
+    )
+
+  rlang::set_names(x, nm)
+}
+
+#' @name is_same_unit_type
+#' @rdname as_unit
+#' @export
+is_same_unit_type <- function(x, y, recurse = FALSE) {
+  as_unit_type(x, recurse) == as_unit_type(y, recurse)
+}
--- R/utils.R
+++ R/utils.R
@@ -26,36 +26,6 @@ utils::globalVariables(
 # @staticimports pkg:isstatic
 #  is_unit is_gg
 
-#' Are x and y the same grid unit value?
-#'
-#' @noRd
-#' @importFrom grid unit unitType
-gg_is_same_unit <- function(x, y) {
-  if (is.character(x)) {
-    x <- grid::unit(1, x)
-  }
-
-  if (is.character(y)) {
-    y <- grid::unit(1, y)
-  }
-
-  grid::unitType(x) == grid::unitType(y)
-}
-
-#' @noRd
-#' @importFrom grid unit convertUnit
-gg_convert_unit <- function(x, from = NULL, to = NULL, type_from = "dimension", value_only = FALSE, ...) {
-  if (is.numeric(x) && is.character(from)) {
-    x <- grid::unit(x, from)
-  }
-
-  stopifnot(
-    is_unit(x)
-  )
-
-  grid::convertUnit(x, unitTo = to, valueOnly = value_only, typeFrom = type_from, ...)
-}
-
 #' Helper to apply an additional ggproto object to each item in a list of ggplot
 #' plots
 #'
@@ -72,11 +42,10 @@ map_gg <- function(.x, gg = NULL) {
 #' Helper to passed family as fixed aesthetic conditionally
 #'
 #' @noRd
-geom_text_if_family <- function(
-    data,
-    mapping =  ggplot2::aes(x = x, y = y, label = label),
-    family = NULL,
-    ...) {
+geom_text_if_family <- function(data,
+                                mapping = ggplot2::aes(x = x, y = y, label = label),
+                                family = NULL,
+                                ...) {
   if (!is.null(family)) {
     ggplot2::geom_text(
       data = data,
@@ -91,4 +60,3 @@ geom_text_if_family <- function(
     )
   }
 }
-
