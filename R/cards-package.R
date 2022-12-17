5e9cbc026f19b962bf1942ada361c9fbcf66ad75
Eli Pousson <eli.pousson@gmail.com>
2022-12-17 11:04:21 -0500

    refactor: update NAMESPACE

--- R/cards-package.R
+++ R/cards-package.R
@@ -2,7 +2,7 @@
 "_PACKAGE"
 
 ## usethis namespace: start
-#' @importFrom rlang on_load local_use_cli run_on_load caller_arg
-#' @importFrom cli cli_abort
+#' @importFrom rlang on_load local_use_cli run_on_load caller_arg %||%
+#' @importFrom cli cli_abort cli_warn
 ## usethis namespace: end
 rlang::on_load(rlang::local_use_cli())
