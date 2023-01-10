ea34ac83c2597ab5e6decb6f54d6814f907bad53
Eli Pousson <eli.pousson@gmail.com>
2023-01-09 22:06:39 -0500

    feat: add margins + get_margin + is_margin functions

---
+++ R/margins.R
@@ -0,0 +1,108 @@
+
+#' Specify the margins of a page or element
+#'
+#' An extended version of [ggplot2::margin()] with more flexibility in
+#' specification. If margin is a margin class object it is returned as is. If
+#' margin is length 1, the t, r, b, and l values are all set to that value. If
+#' margin is length 2, the t and b are set to half the first value and the r and
+#' l are set to half the second value. The ... parameters also allow you to use
+#' the same syntax as margin.
+#'
+#' @param margin A numeric list or vector or a margin class object. For
+#'   [get_margin()] only, margin can be a margin name from `page_extras$margins`.
+#'   Defaults to `NULL`.
+#' @param ... Additional numeric values combined with margin if provided.
+#' @param unit Default units for margins (ignored if margin is a margin class
+#'   object). Passed to as_unit_type() so units class objects as well as unit
+#'   names supported [grid::unit()] are allowed. Defaults to "in".
+#' @examples
+#'
+#' margins(1, unit = "in")
+#'
+#' margins(c(2, 2), unit = "in")
+#'
+#' margins(list(1, 1, 1.5, 1), unit = "cm")
+#'
+#' margins(t = 1, r = 3, b = 1.5, l = 3, unit = "in")
+#'
+#' standard_margin <- get_margin("standard", unit = "in")
+#'
+#' is_margin(standard_margin)
+#'
+#' standard_margin
+#'
+#' @export
+#' @importFrom rlang has_name
+#' @importFrom cli cli_abort
+margins <- function(margin = NULL, ..., unit = "in") {
+  if (!missing(...)) {
+    margin <- c(margin, ...)
+  }
+
+  if (is_margin(margin)) {
+    return(margin)
+  }
+
+  if (is.list(margin)) {
+    margin <- unlist(margin)
+  }
+
+  if (all(rlang::has_name(margin, c("t", "r", "b", "l")))) {
+    margin <- c(margin["t"], margin["r"], margin["b"], margin["l"])
+  }
+
+  if (length(margin) == 1) {
+    margin <- rep(margin, 4)
+  }
+
+  unit <- as_unit_type(unit)
+
+  if (length(margin) == 4) {
+    return(margin(margin[1], margin[2], margin[3], margin[4], unit))
+  }
+
+  if (length(margin) == 2) {
+    margin <- margin / 2
+    return(margin(margin[1], margin[2], margin[1], margin[2], unit))
+  }
+
+  cli::cli_abort(
+    "{.arg margin} must be a length 1, 2, or 4 {.cls numeric} vector."
+  )
+}
+
+#' @name is_margin
+#' @rdname margins
+#' @param x Object to check for class margin and unit
+#' @export
+is_margin <- function(x) {
+  inherits(x, "margin") & inherits(x, "unit")
+}
+
+#' @name margin
+#' @rdname margins
+#' @source ggplot2 package
+#' @export
+margin <- function(t = 0, r = 0, b = 0, l = 0, unit = "pt") {
+  u <- unit(c(t, r, b, l), unit)
+  class(u) <- c("margin", class(u))
+  u
+}
+
+#' @name get_margin
+#' @rdname margins
+#' @export
+get_margin <- function(margin = NULL, ..., unit = "in") {
+  if (is_margin(margin)) {
+    return(margin)
+  }
+
+  if (is.character(margin)) {
+    margin <- rlang::arg_match(margin, unique(page_extras$margins$name))
+    margin <- filter_data(page_extras$margins, margin, "name")
+    margin <- filter_data(margin, unit)
+    margin <- c(margin["t"], margin["r"], margin["b"], margin["l"])
+  }
+
+  margins(margin, ..., unit)
+}
