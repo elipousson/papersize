9c6132d1aff4478159fb3c552aed8dac05ef6570
Eli Pousson <eli.pousson@gmail.com>
2023-01-18 23:11:40 -0500

    feat: add flipped option for y / x aspect ratios
    - feat: add flipped option for y / x aspect ratios  + sep option to improve handling of character asp values
    - feat: add cols argument to improve handling of page data.frame inputs

--- R/as_asp.R
+++ R/as_asp.R
@@ -1,49 +1,78 @@
 #' Convert character string, page name, or page data.frame to numeric aspect ratio
 #'
+#' Convert a character vector with an aspect ratio to a numeric aspect ratio or
+#' get the aspect ratio for a given page that has an aspect ratio column.
+#'
 #' @param asp Aspect ratio of width to height as a numeric value (e.g. 0.33) or
 #'   characters separated by a colon (e.g. "1:3"). If numeric, [as_asp()]
 #'   returns the value of asp without modification.
 #' @param page If character, page is passed as the name parameter to
 #'   [get_page_size()].
 #' @inheritParams get_page_size
+#' @param flipped If `TRUE`, return aspect ratio of height to width (y / x),
+#'   instead of width to height.
+#' @param sep Separator character to use if asp is a character vector. Defaults
+#'   to ":".
 #' @inheritDotParams get_page_size
 #' @seealso [isstatic::as_orientation()]
+#' @returns A numeric vector.
 #' @examples
 #' as_asp(8.5 / 11)
 #'
 #' as_asp("11:17")
 #'
-#' as_asp(paper = "letter")
+#' as_asp("3/2", sep = "/")
 #'
-#' as_asp(paper = "letter", orientation = "landscape")
+#' as_asp(page = "letter")
+#'
+#' as_asp(page = "letter", orientation = "landscape")
+#'
+#' as_asp(page = "letter", orientation = "portrait", flipped = TRUE)
+#'
+#' as_asp(page = make_page_size(8.5, 11, "in"), flipped = TRUE)
 #'
 #' @export
 as_asp <- function(asp = NULL,
                    page = NULL,
                    orientation = NULL,
+                   flipped = FALSE,
+                   sep = ":",
+                   cols = c("width", "height"),
                    ...) {
   if (is.null(asp) & is.null(page)) {
     return(asp)
   }
 
   if (is.character(asp)) {
-    w <- as.numeric(str_extract(asp, ".+(?=:)"))
-    h <- as.numeric(str_extract(asp, "(?<=:).+"))
+    w <- as.numeric(str_extract(asp, paste0(".+(?=", sep, ")")))
+    h <- as.numeric(str_extract(asp, paste0("(?<=", sep, ").+")))
     asp <- w / h
   }
 
   if (is.numeric(asp)) {
+    if (isTRUE(flipped)) {
+      return(1 / asp)
+    }
     return(asp)
-  }
-
-  asp_col <- get_asp_col()
-
-  if (is.data.frame(page)) {
-    return(page[[asp_col]])
   }
 
   if (is.character(page)) {
     page <- get_page_size(page, orientation = orientation, ...)
+  }
+
+  asp_col <- get_asp_col()
+
+  if (is.data.frame(page) && all(has_name(page, cols))) {
+    if (isTRUE(flipped)) {
+      return(as.numeric(page[[cols[2]]]) / as.numeric(page[[cols[1]]]))
+    }
+    return(as.numeric(page[[cols[1]]]) / as.numeric(page[[cols[2]]]))
+  }
+
+  if (is.data.frame(page) && has_name(page, asp_col)) {
+    if (isTRUE(flipped)) {
+      return(1 / page[[asp_col]])
+    }
     return(page[[asp_col]])
   }
 }
