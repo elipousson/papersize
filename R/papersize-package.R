08de9495daf044e5500a5bca958744314a820884
Eli Pousson <eli.pousson@gmail.com>
2023-01-18 23:24:37 -0500

    refactor: use tidy_eval + tidy_dependencies
    - refactor: use tidy_eval + tidy_dependencies
    - refactor: add gridExtra, magick, qpdf, and filenamr to Suggests

--- R/papersize-package.R
+++ R/papersize-package.R
@@ -2,7 +2,10 @@
 "_PACKAGE"
 
 ## usethis namespace: start
-#' @importFrom rlang on_load local_use_cli run_on_load caller_arg %||%
+#' @import rlang
 #' @importFrom cli cli_abort cli_warn
+#' @importFrom glue glue
+#' @importFrom lifecycle deprecated
+#' @importFrom rlang on_load local_use_cli run_on_load caller_arg %||%
 ## usethis namespace: end
 rlang::on_load(rlang::local_use_cli())
