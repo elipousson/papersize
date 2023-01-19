a57664bdca77dd6e9cac16a5995f47298f459650
Eli Pousson <eli.pousson@gmail.com>
2023-01-18 23:19:39 -0500

    feat: add theme_page + inset_page_element

---
+++ R/inset_page_element.R
@@ -0,0 +1,87 @@
+
+#' Create an inset with page size dimensions be added on top of the previous plot
+#'
+#'
+#' @param inset_page Page size data.frame to use for inset, Default: `NULL`
+#' @param left,bottom,right,top numerics or units giving the location of the
+#'   outer bounds. If given as numerics and inset_page is NULL, they will be
+#'   converted to npc units. All four are required if inset_page is` NULL`. If
+#'   inset_page is provided, top *or* bottom and left *or* right must be
+#'   provided as the inset element is expected to be the width and height
+#'   defined by inset_page.
+#' @inheritParams patchwork::inset_element
+#' @inheritDotParams convert_unit_type
+#' @inherit patchwork::inset_element return
+#' @examples
+#' \dontrun{
+#' if (interactive() & is_installed("ggplot2")) {
+#'   library(ggplot2)
+#'   p <- ggplot(mpg, aes(displ, fill = class)) +
+#'     geom_bar()
+#'
+#'   ggplot(mpg, aes(displ, hwy, colour = class)) +
+#'     geom_point() +
+#'     inset_page_element(
+#'       p = p,
+#'       inset_page = get_page_size("Poker card", orientation = "landscape"),
+#'       left = unit(1, "in"),
+#'       bottom = unit(1, "in")
+#'     )
+#' }
+#' }
+#' @seealso
+#'  \code{\link[patchwork]{inset_element}}
+#' @rdname inset_page_element
+#' @export
+#' @importFrom rlang check_installed
+#' @importFrom patchwork inset_element
+inset_page_element <- function(p,
+                               inset_page = NULL,
+                               left = NULL,
+                               bottom = NULL,
+                               right = NULL,
+                               top = NULL,
+                               align_to = "panel",
+                               on_top = TRUE,
+                               clip = TRUE,
+                               ignore_tag = FALSE,
+                               ...) {
+  if (!is.null(inset_page)) {
+    pg_unit <- inset_page[[get_units_col()]]
+    pg_w <- as_unit(inset_page[["width"]], pg_unit)
+    pg_h <- as_unit(inset_page[["height"]], pg_unit)
+
+    if (!is.null(left) & is.null(right)) {
+      left <- convert_unit_type(left, to = pg_unit, ...)
+      right <- left + pg_w
+    }
+
+    if (!is.null(bottom) & is.null(top)) {
+      bottom <- convert_unit_type(bottom, to = pg_unit, ...)
+      top <- bottom + pg_h
+    }
+
+    if (!is.null(right) & is.null(left)) {
+      right <- convert_unit_type(right, to = pg_unit, ...)
+      left <- right - pg_w
+    }
+
+    if (!is.null(top) & is.null(bottom)) {
+      top <- convert_unit_type(top, to = pg_unit, ...)
+      bottom <- top - pg_h
+    }
+  }
+
+  rlang::check_installed("patchwork")
+  patchwork::inset_element(
+    p + theme_page(inset_page),
+    left,
+    bottom,
+    right,
+    top,
+    align_to,
+    on_top,
+    clip,
+    ignore_tag
+  )
+}
---
+++ R/theme_page.R
@@ -0,0 +1,25 @@
+#' Modify plot aspect ratio to match a page size
+#'
+#' Wrapper for ggplot2::theme() that passes as_asp() with `flip = TRUE` to the
+#' aspect.ratio argument to force a plot to match the aspect ratio of the
+#' provided page.
+#'
+#' @inheritParams as_asp
+#' @inheritDotParams ggplot2::theme
+#' @return A ggplot `theme` class object with the aspect.ratio argument set to
+#'   match the page height / width.
+#' @seealso
+#'  \code{\link[ggplot2]{theme}}
+#' @rdname theme_page
+#' @export
+theme_page <- function(page, orientation = NULL, ...) {
+  rlang::check_installed("ggplot2")
+  ggplot2::theme(
+    aspect.ratio = as_asp(
+      page = page,
+      orientation = orientation,
+      flip = TRUE
+    ),
+    ...
+  )
+}
