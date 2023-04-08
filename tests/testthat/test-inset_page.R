aef84500d0655a18048f90ac53e4765ce5398b72
Eli Pousson <eli.pousson@gmail.com>
2023-04-07 23:04:30 -0400

    test: fix inset_page test

--- tests/testthat/test-inset_page.R
+++ tests/testthat/test-inset_page.R
@@ -1,16 +1,16 @@
-test_that("multiplication works", {
+test_that("inset_page works", {
   pg <- get_page_size("letter")
-  inset_pg <- inset_page(page)
+  inset_pg <- inset_page(pg)
   expect_identical(
     c(inset_pg[["width"]], inset_pg[["height"]]),
     c(8.3, 10.8)
   )
-  inset_pg <- inset_page(page, pct_inset = .1)
+  inset_pg <- inset_page(pg, pct_inset = .1)
   expect_identical(
     c(inset_pg[["width"]], inset_pg[["height"]]),
     c(7.65, 9.9)
   )
-  inset_pg <- inset_page(page, inset = as_unit(1, "in"))
+  inset_pg <- inset_page(pg, inset = as_unit(1, "in"))
   expect_identical(
     c(inset_pg[["width"]], inset_pg[["height"]]),
     c(6.5, 9)
