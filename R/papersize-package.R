d783da62b98fc7e1dc5c334c37fdda5fe14d7737
your name <eli.pousson@gmail.com>
2024-04-02 11:13:47 -0400

    Drop unneeded function imports for rlang

--- R/papersize-package.R
+++ R/papersize-package.R
@@ -6,13 +6,5 @@
 #' @importFrom cli cli_abort cli_warn
 #' @importFrom glue glue
 #' @importFrom lifecycle deprecated
-#' @importFrom rlang on_load
-#' @importFrom rlang local_use_cli
-#' @importFrom rlang run_on_load
-#' @importFrom rlang caller_arg
-#' @importFrom rlang %||%
-#' @importFrom rlang has_name
-#' @importFrom rlang has_length
-#' @importFrom rlang check_installed
 ## usethis namespace: end
 NULL
